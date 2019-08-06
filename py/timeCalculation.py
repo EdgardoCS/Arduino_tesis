b8748d14842f7fabe3452940641ddac4c02f638b
EdgardoCS <edgardo_ches@hotmail.com>
2019-08-06 19:33:51 -0400

    changes 06-08

--- py/timeCalculation.py
+++ py/timeCalculation.py
@@ -1,74 +1,53 @@
 # calculation of time (in seconds) that elapsed between the stimulation is applied and the VAS
 # score is register
 
-import numpy as np
+import time
 import datetime
+import numpy as np
+import pandas as pd
 from datetime import timedelta
+import matplotlib.pyplot as plt
 
-# set values from each trial duration
-# trial1 = datetime.datetime.strptime('00:00:24', '%H:%M:%S').time()
-trial1 = 24
-trial2 = 7
-trial3 = 3
-trial4 = 2
-trial5 = 1
-trial6 = 1
+# set path
+path = '../data/data_sub.xlsx'
 
-startEach = []
-startTemp = []
-# interval = datetime.datetime.strptime('00:00:15', '%H:%M:%S').time() #grace
-interval = 20
+dataFrame = pd.read_excel(path, header=(2), sheet_name='trials_ex')
+headers = dataFrame.columns
 
-# read data from subject csv
-path1 = '../data/sujetos/001/001_20190801_1057.csv'
-# read the random position of each trial
-path2 = '../data/rnd/trials.csv'
+vasSubjectNeck = []
+vasSubjectForearm = []
+vasSubjectTactor = []
+vasResult1 = []
+vasResult2 = []
+vasResult3 = []
+fields = 11
 
-# extracts only the timestamp from subjects data
-data1 = np.loadtxt(path1, dtype='str', delimiter=';', usecols=1)
-# extracts random
-data2 = np.loadtxt(path2, dtype='int', delimiter=';', usecols = 0)
-# extracts start_time
-data3 = np.loadtxt(path2,dtype = 'str', delimiter = ';', usecols= 1)
+subjects = 3
 
-print(data1)
-print(data2)
-print(data3)
+# print(dataFrame[headers[2]])
+for t in range(0, subjects):
+    for u in range(0, len(dataFrame)):
+        vasResult1.append(dataFrame[headers[t * fields]][u])
+    vasSubjectNeck.append(vasResult1)
+    for u in range(0, len(dataFrame)):
+        vasResult2.append(dataFrame[headers[(t * fields)+4]][u])
+    vasSubjectForearm.append(vasResult2)
+    for u in range(0, len(dataFrame)):
+        vasResult3.append(dataFrame[headers[(t * fields)+8]][u])
+    vasSubjectTactor.append(vasResult3)
+print(vasSubjectNeck)
+print(vasSubjectForearm)
+print(vasSubjectTactor)
 
-# start time for each stimulation
-# only Grace
-#start_time = datetime.datetime.strptime('10:48:15', '%H:%M:%S')
-#startTemp.append(start_time)
 
-# diff_time = startEach[0]+timedelta(seconds = interval)+timedelta(seconds= trial1)
-# print(diff_time)
-#for i in range(0, 30):
-    # print(data2[i])
+#     print(temp)
+#     for i in range(0, len(dataFrame)):
+#         temp = dataFrame[headers[fields*u]][i]
+#         if isinstance(temp, np.float):
+#             vasResult.append(temp)
+#     vasSubjectNeck
+# print(vasResult)
+# print(len(vasResult))
+# print(counter)
 
-    # print(i)
-    # if data2[i] == 3:
-    #     startTemp.append(startTemp[i] + timedelta(seconds=interval) + timedelta(seconds=trial1))
-    # elif data2[i] == 10:
-    #     startTemp.append(startTemp[i] + timedelta(seconds=interval) + timedelta(seconds=trial2))
-    # elif data2[i] == 30:
-    #     startTemp.append(startTemp[i] + timedelta(seconds=interval) + timedelta(seconds=trial3))
-    # elif data2[i] == 50:
-    #     startTemp.append(startTemp[i] + timedelta(seconds=interval) + timedelta(seconds=trial4))
-    # elif data2[i] == 100:
-    #     startTemp.append(startTemp[i] + timedelta(seconds=interval) + timedelta(seconds=trial5))
-    # elif data2[i] == 200:
-    #     startTemp.append(startTemp[i] + timedelta(seconds=interval) + timedelta(seconds=trial6))
-
-#for i in startTemp:
-    #startEach.append(i.time())
-#print(len(startEach))
-# if len(data1) == len(data2):
-# prevent error from incongruent data
-#for u, v in zip(startEach, data1):
-    #print(u)
-    #print(v)
-    #print(u,v)
-
-# # time1 = datetime.datetime.strptime(u, '%H:%M:%S').time()
-
-# elapsed_time = time1-start_time
+plt.figure()
