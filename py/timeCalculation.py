f7110a1b2add2b5419991de939207330fc8465ac
EdgardoCS <edgardo_ches@hotmail.com>
2019-08-06 21:24:55 -0400

    graphs stage 1

--- py/timeCalculation.py
+++ py/timeCalculation.py
@@ -5,6 +5,7 @@ import time
 import datetime
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from datetime import timedelta
 import matplotlib.pyplot as plt
 
@@ -20,25 +21,90 @@ vasSubjectTactor = []
 vasResult1 = []
 vasResult2 = []
 vasResult3 = []
-fields = 11
 
+vas1 = []
+vas2 = []
+vas3 = []
+
+speed1 = []
+speed2 = []
+speed3 = []
+
+fields = 11
 subjects = 3
 
 # print(dataFrame[headers[2]])
 for t in range(0, subjects):
     for u in range(0, len(dataFrame)):
-        vasResult1.append(dataFrame[headers[t * fields]][u])
+        vasResult1.append([dataFrame[headers[t * fields]][u], dataFrame[headers[(t * fields) + 2]][u]])
     vasSubjectNeck.append(vasResult1)
     for u in range(0, len(dataFrame)):
-        vasResult2.append(dataFrame[headers[(t * fields)+4]][u])
+        vasResult2.append([dataFrame[headers[(t * fields) + 4]][u], dataFrame[headers[(t * fields) + 6]][u]])
     vasSubjectForearm.append(vasResult2)
     for u in range(0, len(dataFrame)):
-        vasResult3.append(dataFrame[headers[(t * fields)+8]][u])
+        vasResult3.append([dataFrame[headers[(t * fields) + 8]][u], dataFrame[headers[(t * fields) + 10]][u]])
     vasSubjectTactor.append(vasResult3)
-print(vasSubjectNeck)
-print(vasSubjectForearm)
-print(vasSubjectTactor)
 
+# print(vasSubjectNeck)
+# print(vasSubjectForearm)
+# print(vasSubjectTactor)
+
+for a in range(0, subjects):
+    for b in range(0, len(vasSubjectNeck[a])):
+        # NECK
+        if vasSubjectNeck[a][b][1] == 3:
+            vas1.append(vasSubjectNeck[a][b][0])
+            speed1.append(vasSubjectNeck[a][b][1])
+        if vasSubjectNeck[a][b][1] == 10:
+            vas1.append(vasSubjectNeck[a][b][0])
+            speed1.append(vasSubjectNeck[a][b][1])
+        if vasSubjectNeck[a][b][1] == 30:
+            vas1.append(vasSubjectNeck[a][b][0])
+            speed1.append(vasSubjectNeck[a][b][1])
+        if vasSubjectNeck[a][b][1] == 50:
+            vas1.append(vasSubjectNeck[a][b][0])
+            speed1.append(vasSubjectNeck[a][b][1])
+        if vasSubjectNeck[a][b][1] == 100:
+            vas1.append(vasSubjectNeck[a][b][0])
+            speed1.append(vasSubjectNeck[a][b][1])
+        if vasSubjectNeck[a][b][1] == 200:
+            vas1.append(vasSubjectNeck[a][b][0])
+            speed1.append(vasSubjectNeck[a][b][1])
+
+        # FOREARM
+        if vasSubjectForearm[a][b][1] == 3:
+            vas2.append(vasSubjectForearm[a][b][0])
+            speed2.append(vasSubjectForearm[a][b][1])
+        if vasSubjectForearm[a][b][1] == 10:
+            vas2.append(vasSubjectForearm[a][b][0])
+            speed2.append(vasSubjectForearm[a][b][1])
+        if vasSubjectForearm[a][b][1] == 30:
+            vas2.append(vasSubjectForearm[a][b][0])
+            speed2.append(vasSubjectForearm[a][b][1])
+        if vasSubjectForearm[a][b][1] == 50:
+            vas2.append(vasSubjectForearm[a][b][0])
+            speed2.append(vasSubjectForearm[a][b][1])
+        if vasSubjectForearm[a][b][1] == 100:
+            vas2.append(vasSubjectForearm[a][b][0])
+            speed2.append(vasSubjectForearm[a][b][1])
+        if vasSubjectForearm[a][b][1] == 200:
+            vas2.append(vasSubjectForearm[a][b][0])
+            speed2.append(vasSubjectForearm[a][b][1])
+
+plot1 = pd.DataFrame({'stimulation velocity': speed1, 'VAS score': vas1})
+plot2 = pd.DataFrame({'stimulation velocity': speed2, 'VAS score': vas2})
+
+# plt.subplot(1, 2, 1)
+# sns.swarmplot(x='stimulation velocity', y='VAS score', data=plot1, size=6, color='k')
+# plt.yticks((-10, -5, 0, 5, 10))
+# plt.ylabel("VAS score", labelpad=-5)
+#
+# plt.subplot(1, 2, 2)
+# sns.swarmplot(x='stimulation velocity', y='VAS score', data=plot2, size=6, color='k')
+# plt.yticks((-10, -5, 0, 5, 10))
+# plt.ylabel("VAS score", labelpad=-5)
+
+plt.show()
 
 #     print(temp)
 #     for i in range(0, len(dataFrame)):
@@ -50,4 +116,4 @@ print(vasSubjectTactor)
 # print(len(vasResult))
 # print(counter)
 
-plt.figure()
+# plt.figure()
